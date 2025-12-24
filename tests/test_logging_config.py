5dc008f0fcbb4668268209dc18c76cbc9e8a52c9
Hans Valerie <hansvalerie19@gmail.com>
2025-12-24 08:43:46 +0700

    Remove all test files from tests directory
    Deleted all test files under the tests/ directory, removing unit and integration tests for various modules. This may be in preparation for restructuring, migration, or cleanup.

--- tests/test_logging_config.py
+++
@@ -1,7 +0,0 @@
-from utils.loggingConfig import setup_logging
-
-def test_setup_logging_returns_logger():
-    logger = setup_logging()
-    assert logger.name == "bot"
-    assert len(logger.handlers) == 2
-    assert logger.propagate is False
